2f75ec99f9c33c973d54e23a3dea398c0185d390
Nathan Page <nathanrpage97@gmail.com>
2019-11-14 00:54:09 +0000

    feat: create setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='liquid_crystal',
+    version='0.0.1',
+    author="Nathan Page",
+    author_email='nathanrpage97@gmail.com',
+    description='Pure python driver for I2C LCD display',
+    license='MIT',
+    url='https://github.com/nathanrpage97/liquid-crystal',
+    keywords='LCD display I2C',
+    packages=find_packages()
+)
